33e10a439e72426b6a5d5ca27677c897923055ac
Jaidev Nileshkumar <jaidev.nileshkumar@kcl.ac.uk>
2022-09-25 11:55:05 +0100

    Add a frequency counter

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        try:
+            frequencies[str(item)] += 1
+        except KeyError:
+            frequencies[str(item)] = 1
+        except:
+            print(f"error for item: {item}")
     return frequencies
